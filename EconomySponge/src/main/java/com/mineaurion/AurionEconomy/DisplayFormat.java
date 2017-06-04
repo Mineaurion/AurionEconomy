4f30b7270b4f380e607ce20e6b17ba4e8db5d866
THEJean-Kevin <yoann6defeuro@gmail.com>
2017-06-04 11:41:49 -0400

    remove gradle

--- EconomySponge/src/main/java/com/mineaurion/AurionEconomy/DisplayFormat.java
+++
@@ -1,8 +0,0 @@
-package com.mineaurion.AurionEconomy;
-
-public enum DisplayFormat {
-	LONG, SIGN, SIGNFRONT, SMALL, MAJORONLY;
-
-	private DisplayFormat() {
-	}
-}
--- EconomySponge/src/main/java/com/mineaurion/EconomySpongeMaven/DisplayFormat.java
+++
@@ -1,8 +0,0 @@
-package com.mineaurion.EconomySpongeMaven;
-
-public enum DisplayFormat {
-	LONG, SIGN, SIGNFRONT, SMALL, MAJORONLY;
-
-	private DisplayFormat() {
-	}
-}
